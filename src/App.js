eaeb7efee35b99f893e9bb637caf9ee82753a691
mrff88 <mrff88@gmail.com>
2022-02-23 11:32:00 -0500

    used useReducer and useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer } from "react";
+import "./App.css";
+
+const initialState = {
+  input: 0,
+  output: 0,
+  unit: "",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "changeInput":
+      return { ...state, input: action.payload };
+    case "Celsius":
+      return {
+        ...state,
+        output: ((state.input - 32) * 5) / 9,
+        unit: action.type,
+      };
+    case "Farenheit":
+      return {
+        ...state,
+        output: state.input * (9 / 5) + 32,
+        unit: action.type,
+      };
+    default:
+      break;
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleInputChange = (e) => {
+    dispatch({ type: "changeInput", payload: e.target.value });
+  };
+
+  const handleSelectChange = (e) => {
+    dispatch({ type: e.target.value });
+  };
+
+  useEffect(() => {
+    if (state.unit) {
+      console.log("useEffect");
+      dispatch({ type: state.unit });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state.input]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">TEMPERATURE CONVERTER</h1>
+      <p className="description">
+        Fill the input with the value of the temperature you want to convert,
+        then select the temperature unit you want to convert to
+      </p>
+      <input
+        className="input"
+        value={state.input}
+        onChange={handleInputChange}
+        type="number"
+      />
+      <select
+        className="select"
+        defaultValue={"DEFAULT"}
+        onChange={handleSelectChange}
+      >
+        <option value={"DEFAULT"} disabled>
+          Select a unit
+        </option>
+        <option value="Celsius">°C</option>
+        <option value="Farenheit">°F</option>
+      </select>
+      <div className="container">
+        <div className="conversion left">
+          <label>Celsius: </label>
+          <span>
+            {state.unit === "Celsius" ? `${state.output.toFixed(2)} °C` : " - "}
+          </span>
+        </div>
+        <div className="conversion">
+          <label>Farenheit: </label>
+          <span>
+            {state.unit === "Farenheit"
+              ? `${state.output.toFixed(2)} °F`
+              : " - "}
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
